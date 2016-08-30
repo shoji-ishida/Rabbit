28e61fdcbefae5b8befb14f641c87079687f607a
Shoji Ishida <shoji.ishida@gmail.com>
2016-08-30 17:58:25 +0900

    Use internal hostname Use custom ThreadPoolExecutor for Receive(consumer)

--- src/jp/catalyna/Receive.java
+++ src/jp/catalyna/Receive.java
@@ -3,20 +3,41 @@ package jp.catalyna;
 import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
  * Created by ishida on 2016/08/24.
  */
 public class Receive {
     private final static String QUEUE_NAME = "hello";
+    private final static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
+    private final static String RMQ_HOSTNAME = "dev-mongo01.webpush.internal";
+
+    private final static int MAX_THREADS = 10;
 
     public static void main(String[] argv)
             throws java.io.IOException,
             java.lang.InterruptedException {
 
+        int numOfThreads = MAX_THREADS;
+        try {
+            numOfThreads = Integer.valueOf(argv[0]);
+        } catch (Exception e) {
+
+        }
+
+        log("Thread pool created with " + numOfThreads + "threads");
+        ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
+
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("localhost");
-        Connection connection = factory.newConnection();
+        factory.setHost(RMQ_HOSTNAME);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
+        Connection connection = factory.newConnection(executor);
         Channel channel = connection.createChannel();
 
         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
@@ -27,9 +48,14 @@ public class Receive {
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body)
                     throws IOException {
                 String message = new String(body, "UTF-8");
-                System.out.println(" [x] Received '" + message + "'");
+                log("Received '" + message + "'");
             }
         };
         channel.basicConsume(QUEUE_NAME, true, consumer);
+    }
+
+    public static void log(String str) {
+        Date date = new Date();
+        System.out.println(dateFormat.format(date) +":" + str);
     }
 }
--- src/jp/catalyna/Send.java
+++ src/jp/catalyna/Send.java
@@ -5,8 +5,12 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.Channel;
 
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
+import java.util.logging.Logger;
 
 public class Send {
 
@@ -14,10 +18,13 @@ public class Send {
     private final static int NUM_OF_MSGS = 10000;
     private final static int MAX_THREADS = 10;
     private final static int NUM_OF_CHUNK = 10;
+    private final static String RMQ_HOSTNAME = "dev-mongo01.webpush.internal";
 
     private ConnectionFactory factory;
     private Connection connection;
     private Channel channel;
+    private final static Logger logger = Logger.getLogger("RBMQSend");
+    private final static DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss.SSS");
 
     public static void main(String[] args) throws java.io.IOException {
         int numOfThreads = MAX_THREADS;
@@ -26,18 +33,24 @@ public class Send {
         } catch (Exception e) {
 
         }
-        System.out.println("Thread pool created with " + numOfThreads + "threads");
+
+
+        log("Thread pool created with " + numOfThreads + "threads");
+        //System.out.println("Thread pool created with " + numOfThreads + "threads");
         ExecutorService executor = Executors.newFixedThreadPool(numOfThreads);
 
         for (int i = 0; i < NUM_OF_CHUNK ; i++) {
             Worker worker = new Worker(NUM_OF_MSGS/NUM_OF_CHUNK);
             executor.execute(worker);
         }
+        log("done");
     }
 
     public Send() throws java.io.IOException {
         factory = new ConnectionFactory();
-        factory.setHost("localhost");
+        factory.setHost(RMQ_HOSTNAME);
+        factory.setUsername("admin");
+        factory.setPassword("admin");
         connection = factory.newConnection();
         channel = connection.createChannel();
     }
@@ -53,6 +66,12 @@ public class Send {
         channel.queueDeclare(QUEUE_NAME, true, false, false, null);
         String message = Integer.toString(n);
         channel.basicPublish("", QUEUE_NAME, null, message.getBytes("UTF-8"));
-        System.out.println(" [x] Sent '" + message + "'" + " by " + Thread.currentThread().getName());
+        //logger.info(" [x] Sent '" + message + "'" + " by " + Thread.currentThread().getName());
+        //System.out.println(" [x] Sent '" + message + "'" + " by " + Thread.currentThread().getName());
+    }
+
+    public static void log(String str) {
+        Date date = new Date();
+        System.out.println(dateFormat.format(date) +":" + str);
     }
 }
